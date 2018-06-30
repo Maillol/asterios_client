d4190b4270c90847f259adbc35be7e5c7f1d6eda
Vincent Maillol <vincent.maillol@gmail.com>
2018-06-30 17:50:42 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -1,32 +1,45 @@
-from setuptools import setup
-
-import os
+import ast
+import pathlib
 import sys
-sys.path.insert(0, os.path.join(os.path.curdir, 'asterios_client'))
-
-from __version__ import __version__
+from setuptools import find_packages, setup
 
 
-if 'a' in __version__:
-    development_status = 'Development Status :: 3 - Alpha'
-elif 'b' in __version__:
-    development_status = 'Development Status :: 4 - Beta'
-else:
-    development_status = 'Development Status :: 5 - Production/Stable'
+def read_version():
+    """
+    Read the version from `asterios_client/__version__.py` without import
+    module to avoid side effect.
+    """
+    module_src = (
+        pathlib.Path(__file__).parent / "asterios_client" / "__version__.py"
+    ).read_text()
+    for statment in ast.parse(module_src).body:
+        if isinstance(statment, ast.Assign):
+            if statment.targets[0].id == "__version__":
+                version = statment.value.s
+                break
+    else:
+        exit(
+            "The `__version__` variable is not defined"
+            " in the `asterios_client.__version__` module"
+        )
+
+    return version
 
 
 setup(
-    name='asterios',
-    version=__version__,
+    name='asterios_client',
+    version=read_version(),
     description='Asterios client',
     keywords='escape game server Asterios client',
     author='Vincent Maillol',
     author_email='vincent.maillol@gmail.com',
     url='https://github.com/maillol/asterios_client',
     license='GPLv3',
-    packages=['asterios_client'],
+    packages=['asterios_client'] + [
+        "asterios_client.{}".format(subpackage)
+        for subpackage in find_packages(where="./asterios_client")
+    ],
     classifiers=[
-        development_status,
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
     ],
     python_requires='>=3.5'
